c9fdd22358153d567b0fbf356a504bbdb7f2db82
timothy <tandrian24@bsd.idserve.net>
2024-07-08 11:58:45 +0700

    Initial Commit

---
+++ Nano3_WeatherAppForParent/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Nano3_WeatherAppForParent
+//
+//  Created by Timothy Andrian on 08/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Nano3_WeatherAppForParent/Nano3_WeatherAppForParentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nano3_WeatherAppForParentApp.swift
+//  Nano3_WeatherAppForParent
+//
+//  Created by Timothy Andrian on 08/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Nano3_WeatherAppForParentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
