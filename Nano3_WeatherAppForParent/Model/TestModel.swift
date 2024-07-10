e9793da0873c3da158be8935f17f5013451ff572
Althaf Nafi <althafnaa@gmail.com>
2024-07-10 09:36:01 +0700

    base commit (beneran)

---
+++ Nano3_WeatherAppForParent/Model/TestModel.swift
@@ -0,0 +1,8 @@
+//
+//  TestModel.swift
+//  Nano3_WeatherAppForParent
+//
+//  Created by Althaf Nafi Anwar on 10/07/24.
+//
+
+import Foundation
---
+++ Nano3_WeatherAppForParent/View/Components/CardView.swift
@@ -0,0 +1,8 @@
+//
+//  CardView.swift
+//  Nano3_WeatherAppForParent
+//
+//  Created by Althaf Nafi Anwar on 10/07/24.
+//
+
+import Foundation
